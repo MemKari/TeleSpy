5b4502dcd6d2858f835ee85f72ff5c5bfea9840b
Mariia Zhuravlova <memkari666@gmail.com>
2025-04-11 20:38:26 +0400

    Bugfix: when creating a client, the old telegram version is specified so that the client session is not interrupted.  When sending a message, the sender is now also indicated.

--- src/config.py
+++ src/config.py
@@ -9,8 +9,8 @@ load_dotenv()  # this allows you to take it from the file .env data
 api_id = os.environ.get('api_id')
 api_hash = os.environ.get('api_hash')
 phone = os.environ.get('phone')
+keywords = os.environ.get('keywords')
+result_channel_ID = int(os.environ.get('result_channel_ID', 123))
 
-keywords = set(json.loads(os.environ.get('keywords')))
-result_channel_ID = int(os.environ.get('result_channel_ID'))
-
-print(f"Keywords: {keywords}")
+# print(f"Keywords: {keywords}")
+# print(keywords.split(','))
